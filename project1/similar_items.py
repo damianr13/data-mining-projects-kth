ad676ffba739533f4f90c86ebbc5521608beecb5
Alice De Schutter <alice@mailfence.com>
2021-11-08 09:36:47 +0100

    added comments, can delete later

--- project1/similar_items.py
+++ project1/similar_items.py
@@ -17,146 +17,146 @@ signature_reducing_factor = 100
 
 @udf(returnType=ArrayType(StringType()))
 def shingle_single_document(text):
-	curated_text = re.sub('\s+', ' ', text.strip())
+    curated_text = re.sub('\s+', ' ', text.strip()) # strip each document and remove extra blanks
 
-	shingle_list = [curated_text[i:i+shingle_size] for i in range(0, len(curated_text) - shingle_size)]
-	return list(set(shingle_list))
+    shingle_list = [curated_text[i:i+shingle_size] for i in range(0, len(curated_text) - shingle_size)]
+    return list(set(shingle_list))
 
 def shingle(documents):
-	return documents.withColumn('shingles', shingle_single_document("_2"))
+    return documents.withColumn('shingles', shingle_single_document("_2")) # adds a new column "shingles" to the df with shingles from each document
+
+def parse_arguments(): # set arguments in command line (shingle size and signature reducting factor)
+    global shingle_size, signature_reducing_factor
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shingle-size', help="Set the size of one shingle")
+    parser.add_argument('--signature-reducing-factor', help="Set the number by which the shingle count is divided to obtain signature size")
+    args = parser.parse_args()
+    if args.shingle_size:
+        shingle_size = int(args.shingle_size)
+    if args.signature_reducing_factor:
+        signature_reducing_factor = int(args.signature_reducing_factor)
 
 
-def parse_arguments():
-	global shingle_size, signature_reducing_factor
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--shingle-size', help="Set the size of one shingle")
-	parser.add_argument('--signature-reducing-factor', help="Set the number by which the shingle count is divided to obtain signature size")
-	args = parser.parse_args()
-	if args.shingle_size:
-		shingle_size = int(args.shingle_size)
-	if args.signature_reducing_factor:
-		signature_reducing_factor = int(args.signature_reducing_factor)
-
-
-@udf(returnType=DoubleType())
+@udf(returnType=DoubleType()) #jaccard similarity
 def compare_sets(shingles1, shingles2):
-	shingles_set1 = set(shingles1)
-	shingles_set2 = set(shingles2)
+    shingles_set1 = set(shingles1)
+    shingles_set2 = set(shingles2)
 
-	return len(shingles_set1 & shingles_set2) / len(shingles_set1 | shingles_set2)
+    return len(shingles_set1 & shingles_set2) / len(shingles_set1 | shingles_set2)
 
 
 def shingles_to_signatures(documents):
-	shingle_count = documents.select(explode(documents.shingles)).distinct().count()
+    shingle_count = documents.select(explode(documents.shingles)).distinct().count()
 
-	numHashTables = int(shingle_count / signature_reducing_factor) + 10
-	return Pipeline(stages=[
-			HashingTF(inputCol='shingles', outputCol='hashes'),
-			# TODO: Implement MinHash from scratch based on course literature? 
-			MinHashLSH(inputCol='hashes', outputCol='signature', numHashTables=numHashTables)
-		]).fit(documents).transform(documents)
+    numHashTables = int(shingle_count / signature_reducing_factor) + 10
+    return Pipeline(stages=[
+      HashingTF(inputCol='shingles', outputCol='hashes'),
+      # TODO: Implement MinHash from scratch based on course literature?
+      MinHashLSH(inputCol='hashes', outputCol='signature', numHashTables=numHashTables)
+     ]).fit(documents).transform(documents)
 
 
 def path_to_name(file_name):
-	return file_name.split('/')[-1]
-
+    return file_name.split('/')[-1]
 
 @udf(returnType=ArrayType(StringType()))
 def paths_to_pair(file_name_1, file_name_2):
-	if file_name_1 == file_name_2:
-		return None
-	return sorted([path_to_name(file_name_1), path_to_name(file_name_2)])
+    if file_name_1 == file_name_2:
+        return None
+    return sorted([path_to_name(file_name_1), path_to_name(file_name_2)])
 
 
 def cross_compare(documents, column_name):
-	documents_comparison = documents.selectExpr('_1 as name_1', column_name + ' as ' + column_name + '_1')\
-		.join(documents.selectExpr('_1 as name_2', column_name + ' as ' + column_name + '_2'))
+    documents_comparison = documents.selectExpr('_1 as name_1', column_name + ' as ' + column_name + '_1')\
+     .join(documents.selectExpr('_1 as name_2', column_name + ' as ' + column_name + '_2')) #selectExpr allows for sql expression, selecting column 1 and shingle twice to do cross comparison
 
-	documents_comparison = documents_comparison.withColumn('pair', paths_to_pair('name_1', 'name_2'))\
-		.select('pair', column_name + '_1', column_name + '_2')\
-		.where(col('pair').isNotNull())\
-		.dropDuplicates(['pair'])\
-		.withColumn('similarity', compare_sets(column_name + '_1', column_name + '_2'))\
-		.sort(col('similarity').desc())
+    # first the columns name_1 and name_2 are sorted with paths_to_pair so that we can drop duplicates
+    # compare_sets returns jaccard similarity
+    documents_comparison = documents_comparison.withColumn('pair', paths_to_pair('name_1', 'name_2'))\
+     .select('pair', column_name + '_1', column_name + '_2')\
+     .where(col('pair').isNotNull())\
+     .dropDuplicates(['pair'])\
+     .withColumn('similarity', compare_sets(column_name + '_1', column_name + '_2'))\
+     .sort(col('similarity').desc())
 
-	return documents_comparison.select('pair', 'similarity')
+    return documents_comparison.select('pair', 'similarity')
 
 
 def generate_hash_parameters(documents):
-	random.seed(22223016)
+    random.seed(22223016)
 
-	shingle_count = documents.select(explode(documents.shingles)).distinct().count()
-	print(shingle_count)
-	numHashTables = int(shingle_count / signature_reducing_factor) + 10
+    shingle_count = documents.select(explode(documents.shingles)).distinct().count()
+    print(shingle_count)
+    numHashTables = int(shingle_count / signature_reducing_factor) + 10
 
-	numbersa = random.sample(range(10 ** 6, 10 ** 7), numHashTables)
-	numbersb = random.sample(range(10 ** 5, 10 ** 8), numHashTables)
-	return [[numbersa[i], numbersb[i]] for i in range(numHashTables)]
+    numbersa = random.sample(range(10 ** 6, 10 ** 7), numHashTables)
+    numbersb = random.sample(range(10 ** 5, 10 ** 8), numHashTables)
+    return [[numbersa[i], numbersb[i]] for i in range(numHashTables)]
 
 
 def extract_signature(hashes, minhash_functions):
-	p = minhash_functions[0]
-	return [min([((p[0] * x + p[1]) % 653563) % 262144 for x in hashes.indices.tolist()]) for p in minhash_functions]
+    p = minhash_functions[0]
+    return [min([((p[0] * x + p[1]) % 653563) % 262144 for x in hashes.indices.tolist()]) for p in minhash_functions]
 
 
 def shingles_to_signature_from_scratch(documents):
-	documents_hashed = HashingTF(inputCol='shingles', outputCol='hashes').transform(documents)
-	minhash_params = generate_hash_parameters(documents)
+    documents_hashed = HashingTF(inputCol='shingles', outputCol='hashes').transform(documents)
+    minhash_params = generate_hash_parameters(documents)
 
-	signature_udf = udf(lambda hashes: extract_signature(hashes, minhash_params), returnType=ArrayType(IntegerType()))
+    signature_udf = udf(lambda hashes: extract_signature(hashes, minhash_params), returnType=ArrayType(IntegerType()))
 
-	return documents_hashed.withColumn('signature', signature_udf('hashes'))
+    return documents_hashed.withColumn('signature', signature_udf('hashes'))
 
 
 def compute_lsh_single_document(signature_list, r):
-	return [hash(tuple(signature_list[i: i + r])) for i in range(0, len(signature_list), r)]
+    return [hash(tuple(signature_list[i: i + r])) for i in range(0, len(signature_list), r)]
 
 
 def compute_lsh_buckets_with_band_size(documents, r):
-	lsh_udf = udf(lambda hashes: compute_lsh_single_document(hashes, r), returnType=ArrayType(IntegerType()))
-	documents_lsh = documents.withColumn('lsh', lsh_udf('signature'))
+    lsh_udf = udf(lambda hashes: compute_lsh_single_document(hashes, r), returnType=ArrayType(IntegerType()))
+    documents_lsh = documents.withColumn('lsh', lsh_udf('signature'))
 
-	return documents_lsh.alias('a').join(documents_lsh.alias('b'), arrays_overlap("a.lsh", "b.lsh"))\
-		.withColumn('pair', paths_to_pair('a._1', 'b._1'))\
-		.where(col('pair').isNotNull())\
-		.dropDuplicates('pair')\
-		.withColumn('similarity', compare_sets('a.signature', 'b.signature'))\
-		.select('pair', 'similarity')
+    return documents_lsh.alias('a').join(documents_lsh.alias('b'), arrays_overlap("a.lsh", "b.lsh"))\
+     .withColumn('pair', paths_to_pair('a._1', 'b._1'))\
+     .where(col('pair').isNotNull())\
+     .dropDuplicates('pair')\
+     .withColumn('similarity', compare_sets('a.signature', 'b.signature'))\
+     .select('pair', 'similarity')
 
 
 def compute_lsh_buckets(documents, t):
-	r = 200
+    r = 200
 
-	return compute_lsh_buckets_with_band_size(documents, r)
+    return compute_lsh_buckets_with_band_size(documents, r)
 
 
 def main():
-	parse_arguments()
-	spark_context = SparkContext("local", "Project 1 - Similar items")
-	spark = SparkSession.builder.getOrCreate()
+    parse_arguments()  # set arguments in command line
+    spark_context = SparkContext("local", "Project 1 - Similar items")
+    spark = SparkSession.builder.getOrCreate() # initializes sql related stuff
 
-	documents_rdd = spark_context.wholeTextFiles("documents/*.txt")
-	documents = documents_rdd.toDF()
+    documents_rdd = spark_context.wholeTextFiles("documents/*.txt")  # reads the directory of text files (documents)
+    documents = documents_rdd.toDF()  # converts documents_rdd to dataframe
 
-	documents_shingled = shingle(documents)
-	documents_shingled.show()
-		
-	documents_comparison = cross_compare(documents_shingled, 'shingles')
-	documents_comparison.show(documents_comparison.count(), False)
+    documents_shingled = shingle(documents) # adds shingle column to the dataframe
+    documents_shingled.show()
 
-	documents_hashed = shingles_to_signatures(documents_shingled)
-	documents_hashed.show()
-	documents_comparison = cross_compare(documents_hashed, 'signature')
-	documents_comparison.show(documents_comparison.count(), False)
+    documents_comparison = cross_compare(documents_shingled, 'shingles') # creates new dataframe for jaccard similarities between documents
+    documents_comparison.show(documents_comparison.count(), False) #.count() to show all rows and not truncate df
 
-	documents_hashed_scratch = shingles_to_signature_from_scratch(documents_shingled)
-	documents_hashed_scratch.show()
-	documents_comparison = cross_compare(documents_hashed_scratch, 'signature')
-	documents_comparison.show(documents_comparison.count(), False)
+    documents_hashed = shingles_to_signatures(documents_shingled)
+    documents_hashed.show()
+    documents_comparison = cross_compare(documents_hashed, 'signature')
+    documents_comparison.show(documents_comparison.count(), False)
 
-	documents_lsh_filtered = compute_lsh_buckets_with_bucket_size(documents_hashed_scratch, 10)
-	documents_lsh_filtered.show(documents_lsh_filtered.count(), False)
+    documents_hashed_scratch = shingles_to_signature_from_scratch(documents_shingled)
+    documents_hashed_scratch.show()
+    documents_comparison = cross_compare(documents_hashed_scratch, 'signature')
+    documents_comparison.show(documents_comparison.count(), False)
+
+    documents_lsh_filtered = compute_lsh_buckets_with_bucket_size(documents_hashed_scratch, 10)
+    documents_lsh_filtered.show(documents_lsh_filtered.count(), False)
 
 
 if __name__ == "__main__":
-	main()
+    main()
