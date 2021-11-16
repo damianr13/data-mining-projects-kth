a1a55f7d3302b99c8a9de6c73b1a85576a1b1ef8
Robert-Andrei Damian <damian.robert.andrei@gmail.com>
2021-11-16 08:41:03 +0100

    Improved performance, created output directory

--- project2/frequent_items.py
+++ project2/frequent_items.py
@@ -1,6 +1,6 @@
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, col, udf, count, array_union, size, array_except, avg
+from pyspark.sql.functions import split, col, udf, count, array_union, size, array_except, avg, monotonically_increasing_id
 from pyspark.sql.types import IntegerType, BooleanType, ArrayType
 
 import argparse
@@ -31,24 +31,47 @@ def create_new_groups(one_item_groups, previous_groups, previous_group_size):
 		.filter(size(col('group')) == previous_group_size + 1)
 
 
-def find_supported_groups(baskets, groups):
+def aggregate_and_filter_groups(crossed):
+	print(f'Checking for {crossed.count()} pairs. Aiming for {support_size} support!')
+	return crossed.dropDuplicates(['index', 'group']).groupBy('group').agg(count('group').alias('occurrences'))\
+			.filter(col('occurrences') >= support_size)\
+			.select('group', 'occurrences')
+
+
+def find_supported_groups_multiple_items(helper, group_size):
+	crossed = helper.alias('a').join(helper.alias('b'), on='index', how='inner')\
+		.withColumn('group', array_union('a.group_prev', 'b.group_1'))\
+		.filter(size(col('group')) == group_size)\
+		.select(col('a.index').alias('index'), 
+			col('a.items').alias('items'), 
+			col('a.group_1').alias('group_1'), 
+			col('group'))
+
+	aggregated = aggregate_and_filter_groups(crossed)
+	crossed_useful = aggregated.alias('a').join(crossed, on='group', how='left')\
+		.select(col('index'), col('items'), col('group_1').alias('group_1'), col('a.group').alias('group_prev'))
+
+	return (aggregated, crossed_useful)
+
+
+def find_supported_groups_one_item(baskets, groups):
 	groups_count = groups.count()
-	print(f'Checking for {groups_count} groups. Aiming for {support_size} support!')
 
 	crossed = baskets.join(groups).filter(size(array_except(col('group'), col('items'))) == 0)
-	return crossed.groupBy('group').agg(count('group').alias('occurrences'))\
-		.filter(col('occurrences') >= support_size)\
-		.select('group', 'occurrences')
+	aggregated = aggregate_and_filter_groups(crossed)
+
+	crossed_useful = aggregated.alias('a').join(crossed, on='group', how='left')\
+		.select(col('index'), col('items'), col('a.group').alias('group_1'), col('a.group').alias('group_prev'))
+
+	return (aggregated, crossed_useful)
 
 
-def gather_all_groups(baskets, one_item_groups, previous_groups, previous_group_size):
-	next_groups = create_new_groups(one_item_groups, previous_groups, previous_group_size)
-
-	supported_groups = find_supported_groups(baskets, next_groups) 
+def gather_all_groups(helper, previous_groups, previous_group_size):
+	(supported_groups, helper) = find_supported_groups_multiple_items(helper, previous_group_size + 1) 
 	supported_group_count = supported_groups.count()
 	print(f'Identified {supported_group_count} new supported groups!')
 	if supported_group_count > 0:
-		recursive_supported_groups = gather_all_groups(one_item_groups, supported_groups, previous_group_size + 1)
+		recursive_supported_groups = gather_all_groups(helper, supported_groups, previous_group_size + 1)
 		return previous_groups.union(recursive_supported_groups)
 
 	return previous_groups
@@ -64,6 +87,7 @@ def main():
 
 	to_numeric = udf(lambda x: [int(i) for i in x if i.isnumeric()], ArrayType(IntegerType()))
 	baskets = baskets.select(split(col('value'), ' ').alias('items')).select(to_numeric(col('items')).alias('items'))
+	baskets = baskets.withColumn('index', monotonically_increasing_id())
 	baskets.show()
 
 	last_item_id = find_last_item_id(baskets)
@@ -73,10 +97,12 @@ def main():
 	items_df = spark.range(0, last_item_id + 1).toDF('id').withColumn('group', vectorize('id')).select('group')
 	items_df.show()
 
-	one_item_groups = find_supported_groups(baskets, items_df)
+	one_item_groups, helper = find_supported_groups_one_item(baskets, items_df)
 
-	all_supported_groups = gather_all_groups(baskets, one_item_groups, one_item_groups, 1)
+	all_supported_groups = gather_all_groups(helper, one_item_groups, 1)
 	all_supported_groups.show()
+	all_supported_groups.withColumn('group_size', size(col('group'))).groupBy('group_size').agg(count(col('group_size'))).show()
+	all_supported_groups.write.mode("overwrite").format("json").save(f'data/output-{support_size}')
 
 
 if __name__ == "__main__":
@@ -89,4 +115,10 @@ if __name__ == "__main__":
 3. make combinations with items in step 3 and the items in step 1
 ...
  until "combinations in step n" is an empty set
+
+
+
+
+Idea: after we have the table with length one groups, we merge it with itself, so we don't associate every 
+possible group with every basket, but we build on the (basket, group) relation already known
 """
