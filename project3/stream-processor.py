0280882d66bac53b2f0dae4c1cd7e8b00b31c072
Alice De Schutter <alice@mailfence.com>
2021-11-25 16:33:34 +0100

    project3

--- project3/stream-processor.py
+++ project3/stream-processor.py
@@ -2,63 +2,68 @@ from kafka import KafkaConsumer
 from collections import defaultdict
 import random
 
-CONST_MAX_EDGE_COUNT = 10000
+CONST_MAX_EDGE_COUNT = 10000 # number edges that is in memory
 
-edgeSet = set()
+edgeSet = set() # useful to select edge at random
 counters = defaultdict(int)
 
 
-def sampleEdge(rawPair, t):
-	if t <= CONST_MAX_EDGE_COUNT:
-		return True
+def sampleEdge(t):
+    if t <= CONST_MAX_EDGE_COUNT: # t <= max memory
+        return True # save edge
 
-	coinflip = random.randint(1, t)
-	if coinflip < CONST_MAX_EDGE_COUNT:
-		removedPair = random.choices(list(edgeSet))[0]
-		edgeSet.remove(removedPair)
+    # t > max memory
+    # reservoir sampling
+    coinflip = random.randint(1, t) # Max_memory / t chance of entrance
+    if coinflip < CONST_MAX_EDGE_COUNT:
+        removedPair = random.choices(list(edgeSet))[0] # select pair to remove
+        edgeSet.remove(removedPair)  # remove pair at random
 
-		updateCounters(-1, removedPair[0], removedPair[1])
-		return True
+        updateCounters(-1, removedPair[0], removedPair[1])
+        return True  # save edge
 
-	return False  
-
-
-def main():
-	random.seed(38)
-	consumer = KafkaConsumer('DataStreamsAssignment', bootstrap_servers=['localhost:9092'])
-
-	t = 0
-	for msg in consumer:
-		t += 1
-		pair = tuple(sorted([int(u) for u in msg.value.decode('utf-8').split('\t')]))
-		if pair in edgeSet:
-			continue
-
-		if sampleEdge(msg, t):
-			edgeSet.add(pair)
-			updateCounters(+1, pair[0], pair[1])
+    return False
 
 
 def buildNodeNeighbourhood(u):
-	return set([pair[0] for pair in edgeSet if pair[1] == u]).union(set([pair[1] for pair in edgeSet if pair[0] == u]))
+    return set([pair[0] for pair in edgeSet if pair[1] == u]).union(set([pair[1] for pair in edgeSet if pair[0] == u]))
 
 
-def updateCounters(step, u, v):	
-	nuv = buildNodeNeighbourhood(u).intersection(buildNodeNeighbourhood(v))
+def updateCounters(step, u, v): # update triangle counters
+    # take all the neighbors of u in S --> intersect with all neighbors of v in S (common neighbors)
+    # stores all the nodes included in triangle with edge between u and v (refered to as c)
+    nuv = buildNodeNeighbourhood(u).intersection(buildNodeNeighbourhood(v))
 
-	for c in nuv:
-		counters['global'] += step
-		counters[c] += step
-		counters[u] += step
-		counters[v] += step
+    for c in nuv:
+        counters['global'] += step # triangles in whole graph
+        counters[c] += step
+        counters[u] += step
+        counters[v] += step
 
-	for k, v in dict(counters).items(): # make a copy of the counters dict to avoid iterating and deleting
-		if v == 0:
-			del counters[k]
+    # remove all keys for which value is 0
+    for k, v in dict(counters).items(): # make a copy of the counters dict to avoid iterating and deleting
+        if v == 0:
+            del counters[k]
 
-	if len(nuv) > 0:
-		print(f'New global triangle count: {counters["global"]}')
+    if len(nuv) > 0:
+        print(f'New global triangle count: {counters["global"]}')
+
+def main():
+    random.seed(38)
+    consumer = KafkaConsumer('DataStreamsAssignment', bootstrap_servers=['localhost:9092'])  # consumer for Kafka
+
+    t = 0  # count of messages received
+    for msg in consumer:
+
+        pair = tuple(sorted([int(u) for u in msg.value.decode('utf-8').split('\t')]))
+        if pair in edgeSet: # not do anything if edge already in memory
+            continue
+
+        t += 1
+        if sampleEdge(t):
+            edgeSet.add(pair)
+            updateCounters(+1, pair[0], pair[1])
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- project3/stream-simulator.py
+++ project3/stream-simulator.py
@@ -3,11 +3,10 @@ from kafka import KafkaProducer
 import threading
 import random
 
-
 CONST_THREAD_COUNT = 4
 CONST_TOPIC = 'DataStreamsAssignment'
 
-
+# stop execution or not
 class CancellationFlag:
 	def __init__(self):
 		self.isCancelled = False
@@ -15,34 +14,18 @@ class CancellationFlag:
 	def cancel(self):
 		self.isCancelled = True
 
-
-def isVeryHelpful(edge, knownNodes):
-	edgeNodes = [int(node) for node in edge.split('\t')]
-	return edgeNodes[0] in knownNodes and edgeNodes[1] in knownNodes
-
-
+# producer: kafka obj
+# graph: lines
+# index: index of thread (every thread will take lines assigned to it...)
 def sendGraphsMessages(producer, graph, index, cancellationFlag):
-	knownNodes = set()
-	while not cancellationFlag.isCancelled:
-		randomness = random.randint(0, 9)
-		if randomness <= -1 and len(knownNodes) > 0: # operation are edges between 2 known nodes
-			pair = random.choice(list(filter(lambda x: isVeryHelpful(x, knownNodes), graph))).strip()
-		elif randomness <= -1 and len(knownNodes) > 0: # operations contain at least one known node
-			targetNode = str(random.choice(list(knownNodes)))
-			pair = next(filter(lambda x: x.startswith(targetNode + '\t') or x.endswith('\t' + targetNode), graph), None)
-			if not pair: # if no pair starting with given node was found, move on
-				continue
+    while not cancellationFlag.isCancelled: # runs when flag is false
 
-			pair = pair.strip()
-		else: # operation are random insertions of edges and nodes
-			edgeIndex = min(random.randrange(0, len(graph), CONST_THREAD_COUNT) + index, len(graph) - 1)
-			pair = graph[edgeIndex].strip()
+        edgeIndex = min(random.randrange(0, len(graph), CONST_THREAD_COUNT) + index, len(graph) - 1)
+        pair = graph[edgeIndex].strip() # remove \n
+        producer.send(CONST_TOPIC, value=bytes(pair, 'utf-8')) # send to Kafka port
 
-		knownNodes.update([int(node) for node in pair.split('\t')])
-		producer.send(CONST_TOPIC, value=bytes(pair, 'utf-8'))
-
-		sleepTime = random.randrange(5, 40) / 100
-		sleep(sleepTime)
+    	sleepTime = random.randrange(5, 40) / 100
+    	sleep(sleepTime)
 
 
 def main():
